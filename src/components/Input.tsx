c345c188f8f1887cf6dae24b596204307e845392
Leandro Andrade de Oliveira <leandro.andrade.d.oliveira@gmail.com>
2022-10-27 18:23:50 -0300

    Creating input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export function Input(props: InputProps) {
     return (
-        <div className="flex items-center p-4 rounded w-full bg-gray-500 focus-within:ring-1 ring-purple-dark">
+        <div className="flex items-center p-4 rounded-lg w-full bg-gray-500 focus-within:ring-1 ring-purple-dark">
             <input
                 type="text"
                 className="flex-1 bg-transparent outline-none text-gray-100 text-lg placeholder:text-gray-300"
